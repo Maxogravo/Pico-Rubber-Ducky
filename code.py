4f69a3329d933729237c587363131b1ecd7a4e17
MG-sec <22gravesma@christletonhigh.co.uk>
2025-10-03 09:40:51 +0100

    Update code.py

--- code.py
+++ code.py
@@ -6,17 +6,10 @@ from adafruit_hid.keycode import Keycode
 kbd = Keyboard(usb_hid.devices)
 
 def Attack():
-    kbd.send(Keycode.CONTROL, Keycode.ALT, Keycode.T)
-    time.sleep(1)
-    kbd.send(Keycode.F)
-    kbd.send(Keycode.A)
-    kbd.send(Keycode.S)
-    kbd.send(Keycode.T)
-    kbd.send(Keycode.F)
-    kbd.send(Keycode.E)
-    kbd.send(Keycode.T)
-    kbd.send(Keycode.C)
-    kbd.send(Keycode.H)
-    kbd.send(Keycode.ENTER)
+   instruction = input(“Enter instruction: ”)
+   for char in instruction:
+      char = “Keycode.” + char
+      kbd.send(char)
+   kbd.send(Keycode.ENTER)
 
 Attack()
