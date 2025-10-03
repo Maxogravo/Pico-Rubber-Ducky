389acb379626d07b40410f0be4429769eaaa600e
MG-sec <22gravesma@christletonhigh.co.uk>
2025-10-03 13:08:58 +0100

    Update code.py

--- code.py
+++ code.py
@@ -4,12 +4,10 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
 kbd = Keyboard(usb_hid.devices)
+instruction = input("Enter Instruction: ")
 
-def Attack():
-   instruction = input(“Enter instruction: ”)
-   for char in instruction:
-      char = “Keycode.” + char
-      kbd.send(char)
-   kbd.send(Keycode.ENTER)
+for char in instruction:
+    char = "Keycode." + char.upper()
+    kbd.send(char)
 
-Attack()
+kbd.send(Keycode.ENTER)
